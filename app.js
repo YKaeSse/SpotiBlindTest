5bd6ded25a07370019da73ceb02df0d633c3d13c
Dginos <elios.p59@gmail.com>
2023-11-21 21:05:20 +0100

    fix: app

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 // server.js
 const express = require('express');
 const path = require('path');
-const history = require('connect-history-api-fallback');
 
 const app = express();
 
-// Use the history API fallback middleware
-app.use(history());
 
 
 // Définir le dossier des fichiers de développement
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'dist/inst-api')));
 
 // Redirection de toutes les requêtes vers le fichier index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/inst-api', 'index.html'));
 });
 
 // Démarrez le serveur sur le port de votre choix
